3911b068b18c11dec8f91315977057d60fc0f13f
Eric Yeh <eric91343@gmail.com>
2022-06-18 18:54:09 +0800

    加入專案檔案。

---
+++ IQueryableInvokeReplacer/ParameterReplaceVisitor.cs
@@ -0,0 +1,20 @@
+﻿// Reference from https://github.com/DogusTeknoloji/BatMap/blob/master/BatMap/ParameterReplaceVisitor.cs
+
+using System.Linq.Expressions;
+
+namespace IQueryableInvokeReplacer;
+
+internal class ParameterReplaceVisitor : ExpressionVisitor
+{
+    private readonly Dictionary<ParameterExpression, Expression> _pairs;
+
+    internal ParameterReplaceVisitor(Dictionary<ParameterExpression, Expression> pairs)
+    {
+        _pairs = pairs;
+    }
+
+    protected override Expression VisitParameter(ParameterExpression node)
+    {
+        return _pairs.TryGetValue(node, out var newPrm) ? newPrm : base.VisitParameter(node);
+    }
+}
